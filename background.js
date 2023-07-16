4e7e98146734f39245bb8f46cc8c9e7b393c6748
Dorian Kita <dorian.kita90@gmail.com>
2023-07-16 13:54:13 +0200

    created background.js module with oop background changing methids

---
+++ background.js
@@ -0,0 +1,29 @@
+
+class Background {
+    constructor() {
+        this.darkMode = document.querySelector(".fa-toggle-off");
+        this.lightMode = document.querySelector(".fa-toggle-on");
+        this.wrapper = document.querySelector(".wrapper");
+        this.wrapper.style.background = "rgb(255,255,255)";
+        this.wrapper.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 10%, rgba(255, 190, 255, 1) 50%, rgba(255, 255, 255, 1) 90%)";
+    }
+
+    blackMode = () => {
+        this.darkMode.style = "display: none";
+        this.lightMode.style = "display: block";
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        this.wrapper.style.background = "rgb(0,0,0)";
+        this.wrapper.style.background = "linear-gradient(90deg, rgba(0,0,0,1) 10%, rgba(134,9,200,1) 50%, rgba(0,0,0,1) 90%)";
+    }
+
+    brightMode = () => {
+        this.darkMode.style = "display: block";
+        this.lightMode.style = "display: none";
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        this.wrapper.style.background = "rgb(255,255,255)";
+        this.wrapper.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 10%, rgba(255, 190, 255, 1) 50%, rgba(255, 255, 255, 1) 90%)";
+    }
+}
+
--- todo.js
+++ todo.js
@@ -1,21 +1,27 @@
-const btn = document.querySelector(".fa-toggle-off");
-const wrapperBgc = document.querySelector(".wrapper");
-btn.addEventListener("click", (e) => {
-    e.target.style = "display: none";
-    btn2.style = "display: block";
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-    wrapperBgc.style.background = "rgb(0,0,0)";
-    wrapperBgc.style.background = "linear-gradient(90deg, rgba(0,0,0,1) 10%, rgba(134,9,200,1) 50%, rgba(0,0,0,1) 90%)";
-})
+// const btn = document.querySelector(".fa-toggle-off");
+// const wrapperBgc = document.querySelector(".wrapper");
+// btn.addEventListener("click", (e) => {
+//     e.target.style = "display: none";
+//     btn2.style = "display: block";
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white";
+//     wrapperBgc.style.background = "rgb(0,0,0)";
+//     wrapperBgc.style.background = "linear-gradient(90deg, rgba(0,0,0,1) 10%, rgba(134,9,200,1) 50%, rgba(0,0,0,1) 90%)";
+// })
 
-const btn2 = document.querySelector(".fa-toggle-on");
-btn2.addEventListener("click", (e) => {
-    e.target.style = "display: none";
-    btn.style = "display: block";
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    wrapperBgc.style.background = "rgb(255, 255, 255)";
-    wrapperBgc.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 10%, rgba(255, 190, 255, 1) 50%, rgba(255, 255, 255, 1) 90%)";
+// const btn2 = document.querySelector(".fa-toggle-on");
+// btn2.addEventListener("click", (e) => {
+//     e.target.style = "display: none";
+//     btn.style = "display: block";
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//     wrapperBgc.style.background = "rgb(255, 255, 255)";
+//     wrapperBgc.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 10%, rgba(255, 190, 255, 1) 50%, rgba(255, 255, 255, 1) 90%)";
 
-})
+// })
+const bgc = new Background()
+
+const button = document.querySelector(".fa-toggle-off")
+const button2 = document.querySelector(".fa-toggle-on")
+button.addEventListener("click", bgc.blackMode)
+button2.addEventListener("click", bgc.brightMode)
