15ab0ea7828826d0b31b23fe8a06b897aa4cfbdd
Dorian Kita <dorian.kita90@gmail.com>
2023-07-16 12:57:37 +0200

    added gradient bgc, changing on toggle button

--- todo.js
+++ todo.js
@@ -1,15 +1,21 @@
 const btn = document.querySelector(".fa-toggle-off");
+const wrapperBgc = document.querySelector(".wrapper");
 btn.addEventListener("click", (e) => {
-    e.target.style = "display: none"
-    btn2.style = "display: block"
-    document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"
+    e.target.style = "display: none";
+    btn2.style = "display: block";
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    wrapperBgc.style.background = "rgb(0,0,0)";
+    wrapperBgc.style.background = "linear-gradient(90deg, rgba(0,0,0,1) 10%, rgba(134,9,200,1) 50%, rgba(0,0,0,1) 90%)";
 })
 
 const btn2 = document.querySelector(".fa-toggle-on");
 btn2.addEventListener("click", (e) => {
-    e.target.style = "display: none"
-    btn.style = "display: block"
-    document.body.style.backgroundColor = "white"
-    document.body.style.color = "black"
+    e.target.style = "display: none";
+    btn.style = "display: block";
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    wrapperBgc.style.background = "rgb(255, 255, 255)";
+    wrapperBgc.style.background = "linear-gradient(90deg, rgba(255, 255, 255, 1) 10%, rgba(255, 190, 255, 1) 50%, rgba(255, 255, 255, 1) 90%)";
+
 })
